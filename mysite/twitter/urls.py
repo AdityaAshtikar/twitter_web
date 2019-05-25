322104effe84cf52206bddc62b822a4b848faec2
Aditya Ashtikar <ashtikar.aditya97@gmail.com>
2019-05-25 09:44:23 +0530

    With Training data

--- mysite/twitter/urls.py
+++ mysite/twitter/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index2, name='index2'),
-    path('', views.index, name='index'), # make it index/
-    path('tweets/$', views.main, name='tweets')
+    path('', views.index2, name='index2'),
+    path('tweet', views.index, name='index'),  # make it index/
+    path('tweets/$', views.main, name='tweets'),
+    path('graph', views.graph, name='graph')
 ]
